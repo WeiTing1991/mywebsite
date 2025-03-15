700647136551dc69c6576e378ea3135fa95f435f
WeiTing1991 <>
2025-03-15 20:29:52 +0100

    add docker auth

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 # usr/bin/env bash
 
+gcloud auth configure-docker europe-docker.pkg.dev # login to gcloud docker registry
 docker buildx build --platform linux/amd64 -t europe-docker.pkg.dev/mywebproject-420221/mywebsite/myserver:lastest .
-docker push europe-docker.pkg.dev/mywebproject-420221/mywebsite/myserver:lastest
+gcloud auth login
+gcloud config set project mywebproject-420221
 
+docker push europe-docker.pkg.dev/mywebproject-420221/mywebsite/myserver:lastest
 gcloud run deploy myweb --image=europe-docker.pkg.dev/mywebproject-420221/mywebsite/myserver:lastest \
 --region=europe-west1 --allow-unauthenticated --max-instances=3
